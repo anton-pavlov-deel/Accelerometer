794fa24271190531a2e183de5875297bd31012e3
pavlovanton <panton.anton98@gmail.com>
2018-04-02 21:54:31 +0300

    Added motion types manager

---
+++ imports/api/MotionTypes.js
@@ -0,0 +1,45 @@
+import _ from 'lodash';
+
+export default class MotionTypes {
+  setDefaultMotionTypes() {
+    this.types = [
+      {
+        name: 'calm',
+        threshold: 0.0
+      },
+
+      {
+        name: 'walking',
+        threshold: 4.0
+      },
+
+      {
+        name: 'running',
+        threshold: 10.0
+      }
+    ]
+  }
+
+  addType(name, threshold) {
+    this.types.push({name, threshold});
+    this.types = _.sortBy(this.types, object => (object.threshold));
+  }
+
+  getTypeByName(name) {
+    const index = _.findIndex(this.types, object => (object.name === name));
+
+    return index !== -1 ? this.types[index] : undefined;
+  }
+
+  getTypeByThreshold(threshold) {
+    let resultType = this.types[0];
+
+    this.types.forEach(type => {
+      if (threshold > type.threshold) {
+        resultType = type;
+      }
+    });
+
+    return resultType;
+  }
+}
